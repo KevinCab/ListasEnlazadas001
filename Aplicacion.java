26dcf28f5e3b1c04c5517cb01641cf87f31b49e3
KevinCab <ultracacomon@hotmail.com>
2017-01-11 11:10:22 -0500

    Create Aplicacion.java

---
+++ Aplicacion.java
@@ -0,0 +1,18 @@
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Nodo s=new Nodo("Casa",null);
+		Nodo t=new Nodo("Maria Chun chun",null);
+		Nodo u=new Nodo("Hola",null);
+		
+		SlinkedList lista1 = new SlinkedList();
+		lista1.InsertarAlFinal(s);
+		lista1.InsertarAlFinal(t);
+		lista1.InsertarAlInicio(u);
+		
+		System.out.println(lista1.toString());
+	}
+
+}
